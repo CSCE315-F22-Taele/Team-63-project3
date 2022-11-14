97d9ecf55e48a7f2160b3b0be585390325f46537
eric27n <73068837+eric27n@users.noreply.github.com>
2022-11-13 19:54:15 -0600

    making css style changes

--- project3/src/navbar.js
+++ project3/src/navbar.js
@@ -3,12 +3,14 @@ import {Link} from 'react-router-dom';
 
 function NavBar(){
     return(
-        <ul>
-            <button><Link to = "/">Customer</Link></button>
-            <button><Link to = "/server">Server</Link></button>
-            <button><Link to = "/manager">Manager</Link></button>
-            <button><Link to = "/googlemap">Locations</Link></button>
-        </ul>
+        <div class="navigation">
+            <ul>
+                <a href="/">Customer</a>
+                <a href="/server">Server</a>
+                <a href="/manager">Manager</a>
+                <a href="/googlemap">Locations</a>
+            </ul>
+        </div>
     )
 }
 

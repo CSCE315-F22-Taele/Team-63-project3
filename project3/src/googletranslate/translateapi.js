2efdd75ed8183be5a9d1329b23d2c7e77bf8b092
jianliu200 <jiangyuanliu2002@gmail.com>
2022-11-13 18:25:44 -0600

    finished the salesreport.js

---
+++ project3/src/googletranslate/translateapi.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Translate(){
+    return(
+        <h1>This is for the Google Translate for the translate</h1>
+    )
+}
--- project3/src/manager/salesreport.js
+++ project3/src/manager/salesreport.js
@@ -1,63 +1,38 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react'
-import {Route,Routes,Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Manager from '../manager';
 
 const Salesreport = () =>{
-    var startdate = '2022-10-01'
-    var enddate = '2022-10-02'
-    // var date11 = (new Date (date1+"T00:00"))
-    // var date22 = (new Date (date2+"T00:00"))
-    // console.log(date11)
-    // console.log(date22)
+    const [startdate,setStartDate] = useState('2022-10-03');
+    const [enddate, setEndDate] = useState('2022-10-04');
     console.log(startdate)
     console.log(enddate)
     const [tables, setTable] = useState([])
 
-    // useEffect(() => {
-    //     const callApi = async () =>{
-    //         await axios.get("http://localhost:5000/certainorder",{
-    //             startdate: date1,
-    //             enddate: date2,
-    //         }).then((result) => {
-    //             console.log(result.data)
-    //             setTable(result.data)
-    //         });
-    //     }
-    //     callApi(date1,date2)
-    //   }, [])
 
-      useEffect(() => {
-        const callApi = async () =>{
-            await axios.get(`http://localhost:5000/certainorder/${startdate}/${enddate}`).then((result) => {
-                console.log("It has succesfully got through the query")
-                console.log(result.data)
-                setTable(result.data)
-            });
-        }
-        callApi()
-      }, [])
+    const callApi = async (startdate,enddate) =>{
+        await axios.get(`http://localhost:5000/certainorder/${startdate}/${enddate}`).then((result) => {
+            console.log("It has succesfully got through the query")
+            console.log(result.data)
+            setTable(result.data)
+        });
+    }
+      
 
-      // const getAllvalue = async() =>{
-      //   console.log(new Date(tables[0].orderdate))
-      //   for(var i = 0; i < tables.length;++i){
-      //     var tabledate = new Date(tables[i].orderdate)
+      const change1 = event =>{
+        setStartDate(event.target.value)
+        console.log(startdate)
+      }
 
-      //     // console.log(tabledate);
-      //     // console.log(tabledate)
-      //     if(tabledate>= date11 && tabledate<=date22){
-      //       console.log("This order is on it")
-      //       console.log(tables[i])
-      //       const new_list = []
-      //       new_list.push(...useTable)
-      //       new_list.push(tables[i])
-      //       setUseTable(new_list)
-      //     }
-      //   }
-      //  }
+      const change2 = event =>{
+        setEndDate(event.target.value)
+        console.log(enddate)
+      }
 
       const displayTable = () => {
         // getAllvalue()
+        // callApi()
         if(tables.length === 0){
           return <div>
             <h1>Welcome to Chick-Fil-A</h1>
@@ -76,8 +51,7 @@ const Salesreport = () =>{
               <h1>This is the manager tab</h1>
               <img src="chick.png" />
               <ul>
-                <button><Link to = "salesreport">Server</Link></button>
-                <button><Link to = "manager">Ordering System</Link></button>
+                <button><Link to = "/manager">Ordering System</Link></button>
               </ul>
             </body><table>
                 <thread>
@@ -105,9 +79,19 @@ const Salesreport = () =>{
         }
       }
 
+      function finalResult(){
+        console.log("This is the start date: ",startdate)
+        console.log("This is the enddate: ",enddate)
+        callApi(startdate,enddate)
+        displayTable()
+      }
+
       return(<>
 
         <div>
+            <input onChange={change1} value = {startdate}/>
+            <input onChange={change2} value = {enddate}/>
+            <button onClick = {()=>{finalResult()}}>Submit</button>
             {displayTable()}
         </div>
         </>

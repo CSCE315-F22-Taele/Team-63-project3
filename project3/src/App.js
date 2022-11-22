2a18a10294ef60b3c00e65c07deaec8c28e2780a
annahuangg <annahuang5668@gmail.com>
2022-11-21 20:40:04 -0600

    test oauth

--- project3/src/App.js
+++ project3/src/App.js
@@ -13,8 +13,15 @@ import Salesreport from './manager/salesreport';
 import MapFront from './googlemap/mapfrontend'
 import ExcessReport from './manager/excessreport';
 import FoodUpdate from './manager/foodupdate'
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+
+import {gapi} from 'gapi-script';
+
+
 //function App() {
 const App = () => {
+  const [ profile, setProfile ] = useState([]);
+  const clientid = "7130970063-8l4ukqnaa0o24aiklhbbb8vbo8rpos8a.apps.googleusercontent.com";
   const googleTranslateElementInit = () => {
     new window.google.translate.TranslateElement({ pageLanguage: 'en', 
     layout: window.google.translate.TranslateElement.FloatPosition.TOP_LEFT }, 
@@ -22,14 +29,59 @@ const App = () => {
     }
 
   useEffect(() => {
+    // google oauth
+    const initClient = () => {
+      gapi.client.init({
+          clientId: clientid,
+          scope: ''
+      });
+    };
+    gapi.load('client:auth2', initClient);
+
+    // google traslate
     var addScript = document.createElement('script');
     addScript.setAttribute('src', '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit');
     document.body.appendChild(addScript);
     window.googleTranslateElementInit = googleTranslateElementInit;
   }, [])
+
+  const onSuccess = (res) => {
+    setProfile(res.profileObj);
+    if (res.profileObj.name === 'Anna Huang') 
+      alert('Name check\n' + res.profileObj.name + ' (' + res.profileObj.email + ') has signed in\nGo to '
+          + res.profileObj.name + "'s page AppReact");
+    else if (res.profileObj.email === 'annahuang5668@gmail.com ')
+    alert('Mail check\n' + res.profileObj.name + ' (' + res.profileObj.email + ') has signed in\nGo to '
+    + res.profileObj.name + "'s page AppReact");
+    else
+      alert(' name=' + res.profileObj.name + ' email=' + res.profileObj.email +  ' GoogleID=' + res.profileObj.googleId );
+  
+      //return <redirect to='/AppReact' />
+  
+  };
+
+  const logOut = () => {
+    alert(profile.name + ' has signed out' );
+    setProfile(null);
+  };
+
+  const onFailure = (err) => {
+    console.log('Login in failed', err);
+  };
+
   return (
     <body>
       <div id="google_translate_element"></div>
+      <GoogleLogin
+          clientId={clientid}
+          buttonText="Sign in"
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          cookiePolicy={'single_host_origin'}
+          isSignedIn={true}
+      />
+      
+      <GoogleLogout clientId={clientid} buttonText="Log out" onLogoutSuccess={logOut} />
       <NavBar/>
       <Routes>
         <Route exact path = "/customer" element = {<Customer/>}/>
